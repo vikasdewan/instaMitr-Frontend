3faae41e24e5ac2d9a7fc469c41bbbc9a9b837f4
VikasDewangan <dewanganvikas192@gmail.com>
2024-10-31 01:55:29 +0530

    done with basic sigup and login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,9 +6,9 @@ import { useState } from 'react'
 import {toast} from "sonner"
 import axios from 'axios'
 
-function Signup() {
+function Login() {
 
-    const [input , setInput] =  useState({username:"", password:"", email:""});
+    const [input , setInput] =  useState({email:"" , password : ""});
 
     const changeEventHandler = (e)=>{
         setInput({...input,[e.target.name] : e.target.value})
@@ -21,7 +21,7 @@ function Signup() {
         console.log(input)
         try {
             setloading(true);
-            const res = await axios.post('http://localhost:8000/api/v1/user/register',input,{
+            const res = await axios.post('http://localhost:8000/api/v1/user/login',input,{
                  headers :{
                     'Content-Type' : 'application/json'
                  },
@@ -35,9 +35,8 @@ function Signup() {
             console.log(error)
             toast.error(error.response.data.message)
             setInput({
-                username:"",
-                password:"",
-                email:""
+                email:"",
+                password:""
             })
         }
         finally{
@@ -50,18 +49,7 @@ function Signup() {
         <form onSubmit={signupHandler} className='shadow-white shadow-lg flex flex-col gap-1 p-7 w-96'>
             <div className='my-4'>
             <h1 className='text-center font-bold text-xl'>InstaMitr</h1>
-            <p className='pl-2 text-sm text-center mt-2  font-bold'>signup to see photos and videos from your Friends</p>
-            </div>
-            <div>
-                <Label className = "font-medium pl-1">Username</Label>
-                <Input
-                type ="text"
-                placeholder="username"
-                name = "username"
-                value = {input.username}
-                onChange={changeEventHandler}
-                className = "text-black focus-visible:ring-transparent my-2 bg-slate-200" 
-                />
+            <p className='pl-2 text-sm text-center font-bold mt-2'>login to see photos and videos from your Friends</p>
             </div>
             <div>
                 <Label className = "font-medium pl-1">Email</Label>
@@ -86,12 +74,12 @@ function Signup() {
                 />
             </div>
 
-            <p className='text-xs text-center text-slate-400'>People who use our service may have uploaded your contact information to Instagrams.  <span className='text-blue-500'>Learn More</span></p>
+            <p className='text-xs text-center text-slate-400' >People who use our service may have uploaded your contact information to Instagrams.  <span className='text-blue-500'>Learn More</span></p>
 
-            <p  className='text-xs text-center  text-slate-400'>By signing up, you agree to our <span className='text-blue-500'>Terms</span> , <span className='text-blue-500'>Privacy Policy</span> and <span className='text-blue-500'>Cookie Policy</span> </p>
+            <p  className='text-xs text-center text-slate-400'>By signing up, you agree to our <span className='text-blue-500'>Terms</span> , <span className='text-blue-500'>Privacy Policy</span> and <span className='text-blue-500'>Cookie Policy</span> </p>
 
 
-            <Button type='submit' className="bg-slate-300 text-black mt-5 hover:bg-white">SignUp</Button>
+            <Button type='submit' className="bg-slate-300 text-black mt-5 hover:bg-white">Login</Button>
 
         </form>
         
@@ -99,4 +87,4 @@ function Signup() {
   )
 }
 
-export default Signup;
+export default Login;
