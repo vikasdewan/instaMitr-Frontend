7b2f8124a09e1a17eefb9d09e3b17a29ff020071
VikasDewangan <dewanganvikas192@gmail.com>
2024-11-01 18:05:41 +0530

    connecting routes of signup and login using react-router-dom(formatted)

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,43 @@
+import "./App.css";
+import Signup from "./components/Signup.jsx";
+import Login from "./components/Login.jsx";
+import Home from "./components/Home.jsx";
+import MainLayout from "./components/MainLayout";
+import Profile from "./components/Profile.jsx";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 
-import './App.css'
-import Signup from './components/Signup.jsx'
-import Login from './components/Login.jsx'
-import Home from './components/Home.jsx'
- import MainLayout from './components/MainLayout'
- import Profile from './components/Profile.jsx'
-import { createBrowserRouter,RouterProvider,Route } from 'react-router-dom'
+const browserRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/profilee",
+        element: <Profile />,
+      },
+    ],
+  },
 
-
-  const browserRouter = createBrowserRouter([
-    {
-      path : "/",
-      element : <MainLayout/>,
-      children:[
-        {
-          path : "/",
-          element : <Home/>
-        },
-        {
-          path : "/profilee",
-          element : <Profile/>
-        },
-      ]
-    },
-
-    {
-      path : "/login",
-      element : <Login/>,
-    },
-    {
-      path : "/signup",
-      element : <Signup/>,
-    }
-  ])
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+]);
 
 function App() {
-   
-
-
   return (
     <>
-      <RouterProvider router={browserRouter}/>
+      <RouterProvider router={browserRouter} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
