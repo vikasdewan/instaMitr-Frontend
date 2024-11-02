4ab6a13eb05d4de6af04eb5e3a05d16e1d25d706
VikasDewangan <dewanganvikas192@gmail.com>
2024-11-02 23:15:52 +0530

    in progress

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -52,6 +52,10 @@ function Post() {
         src="https://images.unsplash.com/photo-1730465447702-caa810acd8e7?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="post_image"
       />
+
+      <div className="">
+    
+      </div>
     </div>
   );
 }
