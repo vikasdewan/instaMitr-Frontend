b48063fc0fcadfeb7ab5be91f07f00179b59a049
VikasDewangan <dewanganvikas192@gmail.com>
2024-11-03 22:44:45 +0530

    in progress in CommentDialog component(formatted)

--- src/components/RightSideBar.jsx
+++ src/components/RightSideBar.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function RightSideBar() {
-  return (
-    <div className='text-black'>RightSideBar</div>
-  )
+  return <div className="text-black">RightSideBar</div>;
 }
 
-export default RightSideBar
+export default RightSideBar;
