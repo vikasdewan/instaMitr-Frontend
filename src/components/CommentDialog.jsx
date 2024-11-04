eb0aa113a6a75b278c7f503dee6e7a1d6d1bc818
VikasDewangan <dewanganvikas192@gmail.com>
2024-11-04 23:18:20 +0530

    some final working is remaing in CommentDialog

--- src/components/CommentDialog.jsx
+++ src/components/CommentDialog.jsx
@@ -21,58 +21,75 @@ function CommentDialog({ openComment, setOpenComment }) {
             />
           </div>
 
-          <div className="w-1/2 flex-col justify-between">
+          <div className="w-1/2 flex flex-col justify-between">
             <div className="flex items-center justify-between p-4 ">
-            <div className="flex gap-3 items-center ga">
-              <Link>
-                <Avatar>
-                  <AvatarImage src="" />
-                  <AvatarFallback className="bg-black text-white">
-                    IM
-                  </AvatarFallback>
-                </Avatar>
-              </Link>
-              <div>
-                <Link className="font-semibold text-xs hover:text-gray-400">
-                  username
-                </Link>{" "}
-                &nbsp;
+              <div className="flex gap-3 items-center ">
+                <Link>
+                  <Avatar>
+                    <AvatarImage src="" />
+                    <AvatarFallback className="bg-black text-white">
+                      IM
+                    </AvatarFallback>
+                  </Avatar>
+                </Link>
+                <div>
+                  <Link className="font-semibold text-xs hover:text-gray-400">
+                    username
+                  </Link>{" "}
+                  &nbsp;
+                </div>
               </div>
-            </div>
 
-            <Dialog>
-          <DialogTrigger asChild>
-            <MoreHorizontal className="cursor-pointer" />
-          </DialogTrigger>
-          <DialogContent className="bg-black text-white flex flex-col items-center text-sm text-center">
-            <Button
-              variant="ghost"
-              className="cursor-pointer w-fit text-[#ED4956] font-bold rounded-xl hover:bg-gray-500"
-            >
-              Unfollow
-            </Button>
-            <Button
-              variant="ghost"
-              className="cursor-pointer w-fit   rounded-xl hover:bg-gray-500"
-            >
-              Add to Favourites
-            </Button>
-            <Button
-              variant="ghost"
-              className="cursor-pointer w-fit  rounded-xl hover:bg-gray-500"
-            >
-              About this account
-            </Button>
-            <Button
-              variant="ghost"
-              className="cursor-pointer w-fit  rounded-xl font-bold hover:bg-gray-500"
-            >
-              Delete
-            </Button>
-          </DialogContent>
-        </Dialog>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <MoreHorizontal className="cursor-pointer" />
+                </DialogTrigger>
+                <DialogContent className="bg-black text-white flex flex-col items-center text-sm text-center">
+                  <Button
+                    variant="ghost"
+                    className="cursor-pointer w-fit text-[#ED4956] font-bold rounded-xl hover:bg-gray-500"
+                  >
+                    Unfollow
+                  </Button>
+                  <Button
+                    variant="ghost"
+                    className="cursor-pointer w-fit   rounded-xl hover:bg-gray-500"
+                  >
+                    Add to Favourites
+                  </Button>
+                  <Button
+                    variant="ghost"
+                    className="cursor-pointer w-fit  rounded-xl hover:bg-gray-500"
+                  >
+                    About this account
+                  </Button>
+                  <Button
+                    variant="ghost"
+                    className="cursor-pointer w-fit  rounded-xl font-bold hover:bg-gray-500"
+                  >
+                    Delete
+                  </Button>
+                </DialogContent>
+              </Dialog>
             </div>
             <hr />
+
+           
+            <div className="flex flex-col flex-grow justify-between">
+              <div className="overflow-y-auto max-h-96 p-4">
+                comments ayenge
+                comments ayenge
+              </div>
+              <div className="p-4 flex">
+                <input
+                  type="text"
+                  placeholder="Add a comment...."
+                  className= "placeholder-white  text-white bg-black text-sm w-full outline-none border-gray-300 p-2 rounded"
+                />
+                <Button variant="outline" className="bg-black text-blue-500 border-none hover:bg-none hover:bg-black hover:text-blue-200 font-bold">Send</Button>
+              </div>
+            </div>
+            
           </div>
         </div>
       </DialogContent>
