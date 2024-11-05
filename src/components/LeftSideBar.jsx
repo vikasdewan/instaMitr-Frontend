3481d2edd0b51fabe1c5490f69399d8f1ecac00e
VikasDewangan <dewanganvikas192@gmail.com>
2024-11-05 21:00:44 +0530

    using redux-persist to make the content or data persist in the frontend during the session

--- src/components/LeftSideBar.jsx
+++ src/components/LeftSideBar.jsx
@@ -13,29 +13,16 @@ import { Avatar, AvatarImage, AvatarFallback } from "./ui/avatar";
 import { toast } from "sonner";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { useSelector } from "react-redux";
 
-const sideBarItems = [
-  { icon: <Home />, text: "Home" },
-  { icon: <Search />, text: "Search" },
-  { icon: <TrendingUp />, text: "Explore" },
-  { icon: <PlaySquare />, text: "Reels" },
-  { icon: <MessageCircle />, text: "Messages" },
-  { icon: <Heart />, text: "Notifications" },
-  { icon: <PlusSquare />, text: "Create" },
-  {
-    icon: (
-      <Avatar className="w-8 h-8">
-        <AvatarImage src="https://github.com/shadcn.png" />
-        <AvatarFallback>IM</AvatarFallback>
-      </Avatar>
-    ),
-    text: "Profile",
-  },
-  { icon: <LogOut />, text: "Logout" },
-];
+
 
 function LeftSideBar() {
   const navigate = useNavigate();
+  const {user} = useSelector(store => store.auth);
+
+  
+
   const logoutHandler = async () => {
     try {
       const res = await axios.get("http://localhost:8000/api/v1/user/logout", {
@@ -54,6 +41,26 @@ function LeftSideBar() {
     console.log(`sidebarHandler called with textType: ${textType}`);
     if (textType === "Logout") logoutHandler();
   };
+
+  const sideBarItems = [
+    { icon: <Home />, text: "Home" },
+    { icon: <Search />, text: "Search" },
+    { icon: <TrendingUp />, text: "Explore" },
+    { icon: <PlaySquare />, text: "Reels" },
+    { icon: <MessageCircle />, text: "Messages" },
+    { icon: <Heart />, text: "Notifications" },
+    { icon: <PlusSquare />, text: "Create" },
+    {
+      icon: (
+        <Avatar className="w-8 h-8">
+          <AvatarImage src={user?.profileImage}  alt="@shadcn"/>
+          <AvatarFallback>IM</AvatarFallback>
+        </Avatar>
+      ),
+      text: "Profile",
+    },
+    { icon: <LogOut />, text: "Logout" },
+  ];
 
   return (
     <div className="bg-black text-white fixed top-0 z-10 left-0 px-4 border-r-2 border-r-gray-700 border-grey-300 w-[16%] h-screen">
