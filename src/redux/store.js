a966349da2f5880ad0bae4acd0ab08542285f511
VikasDewangan <dewanganvikas192@gmail.com>
2024-11-05 20:31:59 +0530

    set redux toolkit for userAuth in the web App(formatted)

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./authSlice.js";
 
 const store = configureStore({
-    reducer:{
-        auth:authSlice.reducer
-    }
-})
+  reducer: {
+    auth: authSlice.reducer,
+  },
+});
 
 export default store;
