618d5fab4053fff1943eb228bfba9b5dc3485cff
VikasDewangan <dewanganvikas192@gmail.com>
2024-11-05 20:34:08 +0530

    create authSlice

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -1,18 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const authSlice = createSlice({
-    name :'auth',
-    initialState:{
-        user:null
+  name: "auth",
+  initialState: {
+    user: null,
+  },
+  reducers: {
+    //actions
+    setAuthUser: (state, action) => {
+      state.user = action.payload;
     },
-    reducers:{
-
-        //actions
-        setAuthUser :(state,action) => {
-            state.user = action.payload;    
-        }
-    }
+  },
 });
 
-export const {setAuthUser} = authSlice.actions;
-export default authSlice.reducer
+export const { setAuthUser } = authSlice.actions;
+export default authSlice.reducer;
