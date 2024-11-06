42184902c234559f791cdcaa8a6afc9d7dcca522
VikasDewangan <dewanganvikas192@gmail.com>
2024-11-06 22:44:15 +0530

    working on Frontend of CreatePostDialog(Formatted)

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,38 +1,41 @@
-import React from 'react'
-import { Dialog, DialogContent, DialogHeader } from './ui/dialog'
-import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import React from "react";
+import { Dialog, DialogContent, DialogHeader } from "./ui/dialog";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
-function CreatePost({open,setOpen}) {
-
-
-    const createPostHandler = async(e)=>{
-        e.preventDefault();
-        try {
-            
-
-        } catch (error) {
-            console.log(error)
-        }
+function CreatePost({ open, setOpen }) {
+  const createPostHandler = async (e) => {
+    e.preventDefault();
+    try {
+    } catch (error) {
+      console.log(error);
     }
+  };
   return (
     <>
-    <Dialog open={open}>
-        <DialogContent onInteractOutside={()=> setOpen(false)} className="bg-black text-white" >
-        <DialogHeader className="flex items-center font-bold text-lg">Create New Post</DialogHeader>
-        <div className='flex gap-3 items-center '>
-            <Avatar >
-                <AvatarImage src="" alt="user_Image"/>
-                <AvatarFallback className="text-white bg-pink-700">IM</AvatarFallback>
+      <Dialog open={open}>
+        <DialogContent
+          onInteractOutside={() => setOpen(false)}
+          className="bg-black text-white"
+        >
+          <DialogHeader className="flex items-center font-bold text-lg">
+            Create New Post
+          </DialogHeader>
+          <div className="flex gap-3 items-center ">
+            <Avatar>
+              <AvatarImage src="" alt="user_Image" />
+              <AvatarFallback className="text-white bg-pink-700">
+                IM
+              </AvatarFallback>
             </Avatar>
             <div>
-                <h1 className='font-semibold text-xs'>Username</h1>
-                <span className='text-gray-600 text-xs' >Bio Here...</span>
+              <h1 className="font-semibold text-xs">Username</h1>
+              <span className="text-gray-600 text-xs">Bio Here...</span>
             </div>
-        </div>
+          </div>
         </DialogContent>
-    </Dialog>
+      </Dialog>
     </>
-  )
+  );
 }
 
-export default CreatePost
+export default CreatePost;
