21455dce92e0de002beee06f4333698504835be5
VikasDewangan <dewanganvikas192@gmail.com>
2024-11-08 10:58:04 +0530

    redux setup for postSlice

--- src/redux/postSlice.js
+++ src/redux/postSlice.js
@@ -1,17 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const postSlice = createSlice({
-    name:'post',
-    initialState:{
-        posts:[],
+  name: "post",
+  initialState: {
+    posts: [],
+  },
+  reducers: {
+    //actions
+    setPosts: (state, action) => {
+      state.posts = action.payload;
     },
-    reducers:{
-        //actions
-        setPosts:(state,action) =>{
-            state.posts = action.payload;
-        }
-    }
-})
+  },
+});
 
-export const {setPosts} = postSlice.actions;
+export const { setPosts } = postSlice.actions;
 export default postSlice.reducer;
