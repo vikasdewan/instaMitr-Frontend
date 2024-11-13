ce363061c6b9da620bc9171be9e4f0389bb5dc73
VikasDewangan <dewanganvikas192@gmail.com>
2024-11-13 19:17:37 +0530

    trying to work on rightsidebar

--- src/components/RightSideBar.jsx
+++ src/components/RightSideBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function RightSideBar() {
-  return <div className="text-black">RightSideBar</div>;
+  return <div className="text-black bg-blue-300">RightSideBar</div>;
 }
 
 export default RightSideBar;
