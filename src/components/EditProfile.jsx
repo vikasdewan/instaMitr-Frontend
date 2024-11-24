d3801907fc1d00fe2c532446f40fa2d78078141d
VikasDewangan <dewanganvikas192@gmail.com>
2024-11-25 02:16:05 +0530

    "Added file input and button to change profile image, and textarea for bio editing in EditProfile component"

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { useSelector } from "react-redux";
+import { Button } from "./ui/button";
+import { Textarea } from "./ui/textarea";
 
 export const EditProfile = () => {
+   const imageRef = useRef(); 
     const {user} = useSelector((store) =>store.auth)
   return (
-    <div className="text-white   flex max-w-2xl mx-auto pl-10">
-      <section>
+    <div className="text-white flex max-w-2xl mx-auto my-4">
+      <section className="flex flex-col gap-6 w-full">
         <h1 className="font-bold text-xl">Edit Profile</h1>
-        <div className="flex item-center gap-2 ">
-          
-            <Avatar className="text-black">
+        <div className="flex item-center justify-between bg-gray-900 p-4 rounded-xl  ">
+         
+          <div className="flex items-center justify-center gap-3">
+            <Avatar className="text-black w-16 h-16">
               <AvatarImage src={user?.profileImage} alt="post_image" />
               <AvatarFallback>IM</AvatarFallback> 
             </Avatar>
@@ -20,6 +24,13 @@ export const EditProfile = () => {
                 {user?.bio || "Bio Here...."}
               </span>
             </div>
+          </div>
+          <input ref={imageRef} type="file" className="hidden"/>
+          <Button onClick={() =>imageRef?.current?.click()} className={`bg-blue-700 mt-2 hover:bg-blue-800`} >Change Photo</Button> 
+        </div>
+        <div>
+            <h1 className="font-bold text-xl">Bio</h1>
+            <Textarea name='bio' className={`bg-black text-white bg-gray-900 focus-visible:ring-transparent`}/>
         </div>
       </section>
     </div>
