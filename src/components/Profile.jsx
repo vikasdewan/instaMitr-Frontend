8d0824d7a147addb47e10f55f0679eeaa941dfbb
VikasDewangan <dewanganvikas192@gmail.com>
2024-12-04 19:19:59 +0530

    "Updated API endpoint URLs from 'instamitr.onrender.com' to 'instamitr-backend.onrender.com' in Profile and useGetUserProfile components."

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -54,7 +54,7 @@ function Profile() {
     try {
       // console.log("follow/unfollow button clicked")
       const response = await axios.post(
-        `https://instamitr.onrender.com/api/v1/user/followorunfollow/${userProfile?._id}`,
+        `https://instamitr-backend.onrender.com/api/v1/user/followorunfollow/${userProfile?._id}`,
         {}, // No body data required
         {
           withCredentials: true, // Send cookies with the request
