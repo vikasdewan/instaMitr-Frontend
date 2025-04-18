940ecb0cc41395be4e7be32655c07a922089b797
VikasDewangan <dewanganvikas192@gmail.com>
2025-04-18 20:11:46 +0530

    `Updated dependencies and fixed various minor issues in components`

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,23 +1,18 @@
 import React from "react";
 import Posts from "./Posts.jsx";
-import Story from "./Story.jsx";
+import StoriesCarousel from "./StoriesCarousel.jsx";
 
 function Feed() {
-  const story = [1, 2, 3, 4, 5, 6, 7];
   return (
     <>
-     <div className="">
-      <div className="md:ml-64">
-        <div className="flex max-w-4xl justify-center gap-3 mt-3 mx-auto md:pl-10">
-          {story.map((s) => (
-            <Story key={s} />
-          ))}
+      <div className="w-full ">
+        <div className="md:pl-56  ">
+          <StoriesCarousel />
+        </div>
+        <div className="flex-1 my-1 flex flex-col items-center md:pl-[30%]">
+          <Posts />
         </div>
       </div>
-      <div className="flex-1 my-1 flex flex-col items-center md:pl-[30%]">
-        <Posts />
-      </div>
-    </div>
     </>
   );
 }
