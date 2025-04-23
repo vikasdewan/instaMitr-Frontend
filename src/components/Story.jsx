7d2964e3fdc72b0db0ee56d11a82227991869eed
VikasDewangan <dewanganvikas192@gmail.com>
2025-04-23 22:55:14 +0530

    story.jsx component

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,24 +1,40 @@
- 
-import React from 'react'
- 
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
-function story() {
-    const { user } = useSelector((store) => store.auth);
+function Story({ user }) {
+  const getInitials = (name) => {
+    if (!name) return "U";
+    return name
+      .split(" ")
+      .map((n) => n[0])
+      .join("")
+      .toUpperCase()
+      .slice(0, 2);
+  };
+
   return (
-    <div>
-         <div className="flex item-center gap-2 mt-3 ">
-        <Link to="#">
-          <Avatar className="text-black bg-red-500 size-13">
-            <AvatarImage src={user?.profileImage} alt="post_image" />
-            <AvatarFallback>IM</AvatarFallback>
-          </Avatar>
-        </Link>
+    <div className="flex flex-col items-center gap-1 min-w-[70px]">
+      <Link to={`/profile/${user?._id}`}>
+        <div className="h-16 w-16 md:h-16 md:w-16 rounded-full p-[2px] bg-gradient-to-tr from-yellow-400 via-pink-500 to-purple-600">
+          <div className="h-full w-full rounded-full bg-white p-[2px]">
+            <Avatar className="h-full w-full">
+              <AvatarImage
+                src={user?.profileImage}
+                alt={user?.username || "User"}
+              />
+              <AvatarFallback>
+                {getInitials(user?.fullName || user?.username)}
+              </AvatarFallback>
+            </Avatar>
+          </div>
         </div>
-         </div>
-  )
+      </Link>
+      <p className="text-xs text-center mt-1 truncate max-w-[70px] md:max-w-[80px]">
+        {user?.username}
+      </p>
+    </div>
+  );
 }
 
-export default story
+export default Story;
