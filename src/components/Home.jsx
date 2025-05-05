9c61dbcf15ee0d91689c722340873af10ad388ab
VikasDewangan <dewanganvikas192@gmail.com>
2025-05-05 20:03:11 +0530

    code check

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,28 +8,31 @@ import Loader from "./Loader.jsx";
 
 const Home = () => {
   const [loading, setLoading] = useState(true);
-  useGetAllPost()
+  useGetAllPost();
   useGetSuggestUsers();
-  
-  useEffect(() => { 
-    // Simulate a delay for loading content, like fetching data 
-    setTimeout(() => { setLoading(false); }, 100); // Adjust the timeout as needed 
-    }, []);
 
+  useEffect(() => {
+    // Simulate a delay for loading content, like fetching data
+    setTimeout(() => {
+      setLoading(false);
+    }, 100); // Adjust the timeout as needed
+  }, []);
 
   return (
     <>
-    {loading ?
-     <Loader /> :
-     <div className="text-white flex">
-      <div className="flex-grow bg-black text-white">
-        <Feed />
-        <Outlet />
-      </div>
-      <RightSideBar />
-    </div>  }
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="text-white flex">
+          <div className="flex-grow bg-black text-white">
+            <Feed />
+            <Outlet />
+          </div>
+          <RightSideBar />
+        </div>
+      )}
     </>
   );
-}
+};
 
 export default Home;
